cff6712501d536a49bf05f1c646bbc99198fa11f
Maks <maksymles@gmail.com>
2018-02-03 13:02:42 +0100

    I added a replacement of the Polish date format to an American if it finds one.

--- match_date.py
+++ match_date.py
@@ -3,12 +3,24 @@ import time
 import datetime
 
 dateRegex = re.compile(r'''(
-    (\d{1,4}) #3/14/2015 03-14-2015 2015/3/14  2015\3\14
+    (\d{1,4}) #3/14/2015 03-14-2015 2015/3/14  03.01.2018
     (\s|-|\.|[/]|\\)?
     (\d{1,2})
     (\s|-|\.|/)?
     (\d{1,4})
 )''', re.VERBOSE)
+
+def changeFormatDate():
+    for i in matches:
+        try:
+            polishDate = datetime.datetime.strptime(i, '%d.%m.%Y')
+            usaDate = datetime.datetime.strftime(polishDate, '%Y/%m/%d')
+            print(usaDate)
+            return
+        except:
+            pass
+    print("I did not find the matching date[dd.mm.yyyy].")
+    return
 
 text = str(pyperclip.paste())
 matches = []
@@ -18,11 +30,9 @@ for groups in dateRegex.findall(text):
 
 if len(matches) > 0:
     pyperclip.copy('\n'.join(matches))
-    print('Skopiowano do schowka: ')
+    print('Copied to the clipboard: ')
     print('\n'.join(matches))
-    # for i in list(matches):
-    #     print("i: ", i)
-    #     print(datetime.datetime.strptime("03-14-2015", '%d-%m-%Y'))
+    changeFormatDate()
 
 else:
-    print('Nie znaleziono żadnej daty.')
+    print('I did not find the matching date.')
